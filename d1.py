ca1ba5cb59a93e658987ee8e61cf71129d2c4099
DM <amdutot@gmail.com>
2022-11-22 20:19:17 +0100

    initialisation

---
+++ d1.py
@@ -0,0 +1,44 @@
+
+from math import *
+
+
+"""Day 1: Division and Square-root
+Write a function called divide_or_square that takes one
+argument (a number), and returns the square root of the number
+if it is divisible by 5, returns its remainder if it is not divisible by
+5. For example, if you pass 10 as an argument, then your function
+should return 3.16 as the square root.
+"""
+
+
+
+def divide_or_square( number ):
+
+    # a number is divisible by 5 if the result of the division ends with 0 or 5.
+    res = number / 5
+    size = len(res)
+    if ....:
+        res = sqrt(number)
+        return res
+    else:
+        res =
+
+
+
+
+"""
+Extra Challenge: Longest Value
+Write a function called longest_value that takes a dictionary
+as an argument and returns the first longest value of the
+dictionary. For example, the following dictionary should return
+– apple as the longest value.
+fruits = {'fruit': 'apple', 'color': 'green'}"""
+
+def longest_value( dict ):
+
+
+
+
+
+
+
